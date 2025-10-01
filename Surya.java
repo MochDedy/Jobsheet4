5ff0550f491218f91cd3706b5187a8e7b4827cdc
MochDedy <mochdedy96@gmail.com>
2025-10-01 09:17:05 +0700

    Punya Surya

---
+++ Surya.java
@@ -0,0 +1,7 @@
+public class Surya {
+    public static void main(String[] args) {
+        System.out.println("Aku Surya");
+        System.out.println("Halo Surya, aku Dedy!");
+        // System.out.println("Aku tampan");
+    }
+}
